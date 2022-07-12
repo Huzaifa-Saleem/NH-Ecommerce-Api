7e22a1b1878bbf9cd84663d58d37f8c15874256d
huzaifa saleem <huzaifasaleem3111@outlook.com>
2022-07-12 17:47:23 +0500

    auth api done

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+//register
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+ try {
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+ } catch (err) {
+    res.status(500).json(err)
+ }
+
+});
+
+module.exports = router;
